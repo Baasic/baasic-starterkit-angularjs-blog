54f9959cfef65181e33079fe51b5e9244ff90091
vlatko <vlatko@mono.hr>
2015-03-02 18:07:26 +0100

    - starting working on building menu tree

--- src/app/navigation/menu-edit-controller.js
+++ src/app/navigation/menu-edit-controller.js
@@ -3,22 +3,57 @@
         function EditMenuCtrl($scope, $state, menuService, pageService) {
             'use strict';
 
+            function isItemInMenu(item) {
+                function findInLevel(compareFunc, level) {
+                    for (var i = 0, l = level.length; i < l; i++) {
+                        var node = level[i];
+
+                        if (compareFunc(node)) {
+                            return {
+                                collection: level,
+                                index: i
+                            };
+                        } else if (node.items) {
+                            var res = findInLevel(compareFunc, node.items);
+                            if (res != null) {
+                                return res;
+                            }
+                        }
+                    }
+
+                    return null;
+                }
+
+                var compareFunc;
+                if (item.id) {
+                    compareFunc = function (node) { return node.pageId == item.id; };
+                }
+
+                return findInLevel(compareFunc, $scope.menu.items);
+            }
+
             var isNew = $state.params.id === undefined;
 
             if (!isNew) {
                 menuService.get($state.params.id)
                     .success(function (menu) {
+                        if (!menu.items) {
+                            menu.items = [];
+                        }
+
                         $scope.menu = menu;
                     })
                     .error(function (error) {
                     });
             } else {
-                $scope.menu = {};
+                $scope.menu = {
+                    items: []
+                };
             }
 
             pageService.find({
                 statuses: ['published'],
-                fields: ['title']
+                fields: ['id', 'title']
             })
                 .success(function (pageList) {
                     $scope.pages = pageList.item;
@@ -85,6 +120,20 @@
                         })
                         .error(function (error) {
                         });
+                }
+            };
+
+            $scope.addPageToMenu = function addPageToMenu(page) {
+                var items = $scope.menu.items;
+                var res = isItemInMenu(page);
+
+                if (res === null) {
+                    items.push({
+                        title: page.title,
+                        pageId: page.id
+                    });
+                } else {
+                    res.collection.splice(res.index, 1);
                 }
             };
         }
