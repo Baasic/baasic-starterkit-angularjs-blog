c90a60949db4e81521a2d7d2df878e14c200bcc8
vlatko <vlatko@mono.hr>
2015-03-10 14:49:38 +0100

    - added menu directive

---
+++ src/app/navigation/menu-directive.js
@@ -0,0 +1,33 @@
+﻿angular.module('myApp')
+    .directive('baasicMenu', [
+        function baasicLogin() {
+            'use strict';
+            var menuName;
+
+            return {
+                restrict: 'AE',
+                scope: false,
+                compile: function (elem, attrs) {
+                    if (attrs.baasicMenu !== undefined &&
+                        attrs.baasicMenu !== null &&
+                        attrs.baasicMenu !== '') {
+                        menuName = attrs.baasicMenu;
+                    } else {
+                        menuName = attrs.name;
+                    }
+                },
+                controller: ['$scope', 'menuService',
+                    function menuCtrl($scope, menuService) {
+                        menuService.find({
+                            searchQuery: 'WHERE name = \'' + menuName + '\''
+                        })
+                            .success(function (menus) {
+                                if (menus.totalRecords > 0) {
+                                    $scope.menu = menus.item[0].items;
+                                }
+                            });
+                    }
+                ]
+            };
+        }
+    ]);
