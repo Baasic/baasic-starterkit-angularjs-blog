a4005f7fa1ca259b92d0702138320f0eb8862fbc
ibasic <ibasic@mono.hr>
2015-08-28 14:12:06 +0200

    - API changed

--- src/app/app.js
+++ src/app/app.js
@@ -15,7 +15,7 @@ angular.module('myBlog', [
     function config($locationProvider, $urlRouterProvider, $stateProvider, baasicAppProvider) {
         'use strict';
 
-        baasicAppProvider.create('starterkit-blog', {
+        baasicAppProvider.create('starterkit-blog-events', {
             apiRootUrl: 'api.baasic.com',
             apiVersion: 'beta'
         });
