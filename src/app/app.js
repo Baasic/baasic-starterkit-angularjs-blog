c754e22cc340558c7cccadcbc331d130283b52af
Andrej Mlinarević <andrej@mono.hr>
2015-09-15 10:20:34 +0200

    App.js change

--- src/app/app.js
+++ src/app/app.js
@@ -16,7 +16,7 @@ angular.module('myBlog', [
     function config($locationProvider, $urlRouterProvider, $stateProvider, baasicAppProvider) {
         'use strict';
 
-        baasicAppProvider.create('starterkit-blog-travel', {
+        baasicAppProvider.create('starterkit-blog', {
             apiRootUrl: 'api.baasic.com',
             apiVersion: 'beta'
         });
