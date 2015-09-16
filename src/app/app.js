8cc679a23b7cefc8dacfebadebbdd32f8f7bffc5
ibasic <ibasic@mono.hr>
2015-09-16 12:33:46 +0200

    - app set to default

--- src/app/app.js
+++ src/app/app.js
@@ -17,7 +17,7 @@ angular.module('myBlog', [
     function config($locationProvider, $urlRouterProvider, $stateProvider, baasicAppProvider) {
         'use strict';
 
-        baasicAppProvider.create('starterkit-blog-events', {
+        baasicAppProvider.create('starterkit-blog', {
             apiRootUrl: 'api.baasic.com',
             apiVersion: 'beta'
         });
