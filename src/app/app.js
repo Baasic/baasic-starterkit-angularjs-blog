bd1fec738cd4c3fbfab145016498c95acbb9d356
Andrej Mlinarević <andrej@mono.hr>
2015-09-17 10:41:18 +0200

    Fixed API call

--- src/app/app.js
+++ src/app/app.js
@@ -17,7 +17,7 @@ angular.module('myBlog', [
     function config($locationProvider, $urlRouterProvider, $stateProvider, baasicAppProvider) {
         'use strict';
 
-        baasicAppProvider.create('starterkit-blog-music', {
+        baasicAppProvider.create('starterkit-blog', {
             apiRootUrl: 'api.baasic.com',
             apiVersion: 'beta'
         });
