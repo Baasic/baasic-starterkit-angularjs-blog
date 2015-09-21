4d047da8dd5ce0380982646f69c71fd6ed49fcc1
Mihael Tomić <mihael@mono.hr>
2015-09-21 17:05:28 +0200

    The issue with one theme deployment process is fixed

--- deploy/deploy-all-themes.cmd
+++ deploy/deploy-all-themes.cmd
@@ -5,10 +5,12 @@ del theme.list /Q
 dir /b "../src/themes/" > theme.list
 cd ..
 
+rd "deploy\starter-kit-deploy\angularjs\starterkit-blog-themes\" /S /Q
+md "deploy\starter-kit-deploy\angularjs\starterkit-blog-themes\" 
+
 if "%all%" == "all" (echo Build all themes and prepare the deploy) 
 
-for /f "usebackq delims=" %%F in (deploy\theme.list) DO (	
-	REM echo "%%F" -OutputDirectory %%~dpF    
+for /f "usebackq delims=" %%F in (deploy\theme.list) DO (		
     call :subroutine "%%F" "%all%"
 )
 cd deploy
@@ -31,60 +33,5 @@ GOTO :End
     :Skip
 
 GOTO :End
-
-echo material-design-lite
-SET /P Proceed=Do you want to proceed (Y/N)?
-if /i {%Proceed%}=={y} (GOTO :NextMDL)
-if /i {%Proceed%}=={yes} (GOTO :NextMDL)
-GOTO :QGastro
-
-:NextMDL
-call gulp dist --theme material-design-lite --baseUrl "/angularjs/starterkit-blog-themes/material-design-lite/"
-
-:QGastro
-echo gastro-thumbnail
-SET /P Proceed=Do you want to proceed (Y/N)?
-if /i {%Proceed%}=={y} (GOTO :NextGastro)
-if /i {%Proceed%}=={yes} (GOTO :NextGastro)
-GOTO :QSpaceMinimal
-
-:NextGastro
-call gulp dist --theme gastro-thumbnail --baseUrl "/angularjs/starterkit-blog-themes/gastro-thumbnail/"
-
-:QSpaceMinimal
-echo space-minimal
-SET /P Proceed=Do you want to proceed (Y/N)?
-if /i {%Proceed%}=={y} (GOTO :NextSpaceMinimal)
-if /i {%Proceed%}=={yes} (GOTO :NextSpaceMinimal)
-GOTO :QSpaceThumb
-
-:NextSpaceMinimal
-call gulp dist --theme space-minimal --baseUrl "/angularjs/starterkit-blog-themes/space-minimal/"
-
-:QSpaceThumb
-echo space-thumbnail
-SET /P Proceed=Do you want to proceed (Y/N)?
-if /i {%Proceed%}=={y} (GOTO :NextSpaceThumb)
-if /i {%Proceed%}=={yes} (GOTO :NextSpaceThumb)
-GOTO :QSpaceTiles
-
-:NextSpaceThumb
-call gulp dist --theme space-thumbnail --baseUrl "/angularjs/starterkit-blog-themes/space-thumbnail/"
-
-:QSpaceTiles
-echo space-tiles
-SET /P Proceed=Do you want to proceed (Y/N)?
-if /i {%Proceed%}=={y} (GOTO :NextSpaceTiles)
-if /i {%Proceed%}=={yes} (GOTO :NextSpaceTiles)
-GOTO :End
-
-:NextSpaceTiles
-call gulp dist --theme space-tiles --baseUrl "/angularjs/starterkit-blog-themes/space-tiles/"
-
-REM SET /P Proceed=Do you want to proceed (Y/N)?
-REM if /i {%Proceed%}=={y} (GOTO :NextSpaceTiles)
-REM if /i {%Proceed%}=={yes} (GOTO :NextSpaceTiles)
-REM GOTO :End
-
 
 :End
