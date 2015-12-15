27105121b89103356ce41960d0166538198c91d6
Ivan Pozderac <ivan.pozderac@mono-software.com>
2015-12-15 11:12:30 +0100

    - add directive for replies

--- src/app/comments/comments-directive.js
+++ src/app/comments/comments-directive.js
@@ -9,7 +9,6 @@ angular.module('baasic.blog')
                 controller: ['$scope', '$state', '$stateParams', '$q', 'baasicBlogService','baasicArticleService',
                     function ($scope, $state, $stateParams, $q, blogService, baasicArticleService) {
                         function loadComments() {
-
                         blogService.comments.find($state.params.slug, {
                             embed: 'replies,replies.user,user',
                             orderBy: 'dateUpdated',
@@ -36,31 +35,7 @@ angular.module('baasic.blog')
                             });
                         }
 
-                        function loadReplies() {
-
-                        blogService.comments.replies.find($scope.articleId, $scope.commentId, {
-                            pageNumber : 1,
-                            orderBy: 'dateUpdated',
-                            orderDirection: 'desc',
-                        })
-                            .success(function parseRepliesList(replies) {
-                                var commentId = $scope.commentId;
-                                var collection = $scope.comments.item;
-
-                                angular.forEach(collection, function(value, i){
-                                    if(value.id === commentId){
-                                        collection[i].replies = replies.item;
-                                    }
-                                });
-                            })
-                            .error(function (error) {
-                                console.log(error); //jshint ignore: line
-                            })
-                            .finally(function () {
-                                $scope.reply = '';
-                                $scope.$root.loader.resume();
-                            });
-                        }
+                        loadComments();
 
                         $scope.resetCommentsForm = function resetCommentsForm(form) {
                             if (form) {
@@ -68,11 +43,12 @@ angular.module('baasic.blog')
                                 form.email.$setUntouched();
                                 form.title.$setUntouched();
                                 form.message.$setUntouched();
-                                loadComments();
                             }
                         };
 
+
                         $scope.saveComments = function saveComments() {
+                            $scope.$root.loader.resume();
                             if ($scope.commentsForm.$valid) {
                                 $scope.$root.loader.resume();
                                 $scope.comments.isNew = true;
@@ -84,58 +60,24 @@ angular.module('baasic.blog')
                                     commentUrl: $state.href('master.blog-detail', {}, { absolute: true }) + '{id}'
                                 };
                             //
-                                $scope.comments.options = options;
-                                baasicArticleService.comments.create($scope.comments);
+                        $scope.comments.options = options;
+                        baasicArticleService.comments.create($scope.comments)
+                            .success(function() {
+
                                 $scope.commentsForm.$setPristine(true);
                                 $scope.commentsForm.$setUntouched(true);
-
-
-                                } else {
-                                    baasicArticleService.comments.update({
-                                        articleId: $scope.articleId
-                                    });
-                                }
+                            })
+                            .error(function(error) {
+                                console.log(error); //jshint ignore: line
+                            })
+                            .finally(function() {
                                 loadComments();
                                 $scope.$root.loader.suspend();
+                            });
                             }
-                        };
-
-                        $scope.saveReplies = function saveReplies(comment, reply) {
-                            $scope.comment = comment;
-                            $scope.reply = reply;
-                            $scope.commentId = $scope.comment.id;
-                            $scope.reply.isNew = true;
-                            if ($scope.reply.isNew) {
-                                $scope.$root.loader.suspend();
-
-                                var options = {
-                                    subscribeAuthor: false,
-                                    commentUrl: 'http://test.com'
-                                };
-
-                                baasicArticleService.comments.replies.create($scope.articleId, {
-                                    commentId: $scope.commentId,
-                                    options: options,
-                                    email: $scope.reply.email,
-                                    reply: $scope.reply.reply,
-                                    author: $scope.reply.author
-                                })
-                                    .success(function () {
-                                        loadReplies();
-                                        $scope.reply.author ="";
-                                        $scope.reply.email ="";
-                                        $scope.reply.reply ="";
-                                    })
-                                    .error(function (error) {
-                                        console.log(error); //jshint ignore: line
-                                    })
-                                    .finally(function () {
-                                        $scope.$root.loader.resume();
-                                    });
-                                }
-                            };
-                            loadComments();
                         }
+                    };
+                }
                 ],
                 templateUrl: 'templates/comments/template-comments.html'
                 };
---
+++ src/app/comments/replies-directive.js
@@ -0,0 +1,73 @@
+angular.module('baasic.blog')
+    .directive('commentReplies', [
+        function commentReplies() {
+            'use strict';
+
+            return {
+                restrict: 'AE',
+                scope: true,
+                controller: ['$scope', '$state', '$stateParams', '$q', 'baasicBlogService','baasicArticleService',
+                    function ($scope, $state, $stateParams, $q, blogService, baasicArticleService) {
+                        function loadReplies() {
+                        blogService.comments.replies.find($scope.articleId,  $scope.commentId, {
+                            orderBy: 'dateUpdated',
+                            orderDirection: 'desc',
+                            pageNumber: 1
+                        })
+                            .success(function parseRepliesList(replies) {
+                                var commentId = $scope.comment.id;
+                                var collection = $scope.comments.item;
+
+                                angular.forEach(collection, function(value, i){
+                                    if(value.id === commentId){
+                                        collection[i].replies = replies.item;
+                                    }
+                                });
+                            })
+                            .error(function (error) {
+                                console.log(error); //jshint ignore: line
+                            })
+                            .finally(function () {
+                                $scope.$root.loader.resume();
+                            });
+                        }
+
+                        $scope.saveReplies = function saveReplies(comment, reply) {
+                            $scope.comment = comment;
+                            $scope.reply = reply;
+                            $scope.commentId = $scope.comment.id;
+                            $scope.reply.isNew = true;
+
+                            if ($scope.reply.isNew) {
+                                $scope.$root.loader.suspend();
+
+                                var options = {
+                                    subscribeAuthor: false,
+                                    commentUrl: 'http://test.com'
+                                };
+
+                                baasicArticleService.comments.replies.create($scope.articleId, {
+                                    commentId: $scope.commentId,
+                                    options: options,
+                                    email: $scope.reply.email,
+                                    reply: $scope.reply.reply,
+                                    author: $scope.reply.author
+                                })
+                                    .success(function () {
+                                        loadReplies();
+                                    })
+                                    .error(function (error) {
+                                        console.log(error); //jshint ignore: line
+                                    })
+                                    .finally(function () {
+                                        $scope.$root.loader.resume();
+                                    });
+                            }
+                        };
+                    }
+                ],
+                templateUrl: 'templates/comments/template-replies.html'
+                };
+            }
+        ]
+    );
